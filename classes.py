a6f99f3e8705239ea34f2a716892b65914c974ad
Clémence Robin <clemence.robinletellier@gmail.com>
2019-08-27 17:11:45 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -88,13 +88,9 @@ class Characters:
     def place(self, level):
         """
         place the characters on a border empty ground
-        if one of the border supposedly empty ground is already occupied by the other character, start again
         """
         self.line, self.col = level.find_entry()
-        level.floor[self.line][self.col] == self.name
-        if level.floor[self.line][self.col] != self.name:
-            self.line, self.col = level.find_entry() # genère toujours des erreurs qq fois : à regler
-            level.floor[self.line][self.col] = self.name
+        level.floor[self.line][self.col] = self.name
 
     def search_items(self, level, line, col):
         """
